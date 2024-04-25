87e4907f1687df0f3f7aeabab7e799a2ccc6b307
cesar_css_love <cg.gtav@gmail.com>
2024-04-25 21:11:26 +0200

    asdd-some javascript

--- src/components/CalculatorM.vue
+++ src/components/CalculatorM.vue
@@ -1,34 +1,37 @@
 <template>
 	<div class="container">
 		<section class="result">
-			<input type="number" />
+			<input readonly v-model="current" type="number" />
 		</section>
 
 		<section class="buttons-calculator">
-			<button>1</button>
-			<button>2</button>
-			<button>3</button>
-			<button>x</button>
+			<button class="button">1</button>
+			<button class="button">2</button>
+			<button class="button">3</button>
+			<button class="button">x</button>
 
-			<button>4</button>
-			<button>5</button>
-			<button>6</button>
-			<button>/</button>
+			<button class="button">4</button>
+			<button class="button">5</button>
+			<button class="button">6</button>
+			<button class="button">/</button>
 
-			<button>7</button>
-			<button>8</button>
-			<button>9</button>
-			<button>-</button>
+			<button class="button">7</button>
+			<button class="button">8</button>
+			<button class="button">9</button>
+			<button class="button">-</button>
 
-			<button>0</button>
-			<button>=</button>
-			<button>C</button>
-			<button>%</button>
+			<button class="button">0</button>
+			<button class="button">=</button>
+			<button class="button">C</button>
+			<button class="button">+</button>
 		</section>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const current = ref(0);
+</script>
 
 <style scoped>
 .container {
@@ -49,6 +52,8 @@
 input {
 	width: 100%;
 	border: none;
+	text-align: center;
+	font-size: 1.5em;
 }
 
 input:focus {
@@ -68,5 +73,10 @@ button {
 	color: rgb(230, 226, 226);
 	font-size: 2em;
 	box-shadow: 2px 2px 10px rgb(50, 50, 50), 2px 2px 10px inset rgb(86, 83, 83);
+	backdrop-filter: blur(7.1px);
+}
+
+button:hover {
+	border: 1px solid rgba(255, 255, 255, 0.3);
 }
 </style>
